b2beba1c996a274b5b4362b7fe62082a5244d2af
Nagarehazh <proyectosexitososya@gmail.com>
2022-10-02 19:21:59 -0500

    Responsive Design for Slider
    Fixed details of responsive design for mobile devices.
    
    Changes to be committed:
    	modified:   client/src/components/Slider/SliderStyles.ts

--- client/src/components/Slider/SliderStyles.ts
+++ client/src/components/Slider/SliderStyles.ts
@@ -19,9 +19,7 @@ height: 100vh;
 display: flex;
 position: relative;
 Overflow: hidden;
-@media ${(props) => props.theme.breakpoints.sm} {
-    display: none;
-}
+
 `;
 
 const Arrow = styled.div<ArrowProps>`
@@ -49,6 +47,11 @@ height: 100%;
 display: flex;
 transition: all 1.5s ease;
 transform: translateX(${(props) => props.slideIndex * -100}vw);
+@media ${(props) => props.theme.breakpoints.sm} {
+  transition: all 0.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+  
+}
 `;
 
 const Slide = styled.div<SlideProps>`
@@ -57,25 +60,42 @@ height: 100vh;
 display: flex;
 align-items: center;
 background-color: #${(props) => props.bg};
+@media ${(props) => props.theme.breakpoints.sm} {
+    display: flex;
+    flex-direction: column;
+}
 `;
 const ImgContainer = styled.div`
 flex: 1;
-height: 100%;
+height: 80%;
+@media ${(props) => props.theme.breakpoints.sm} {
+    height: 50%;
+}
 `;
 
 const Image = styled.img`
-width: 100%;
-height: 80%;
+width: 80%;
+height: 100%;
 obeject-fit: cover;
+
 `;
 
 const InfoContainer = styled.div`
-flex: 1;
+flex: 2;
 padding: 50px;
+@media ${(props) => props.theme.breakpoints.sm} {
+    padding: 20px;
+    text-align: center;
+}
 `;
 
 const Title = styled.h1`
 font-size: 70px;
+max-width: 800px;
+
+@media ${(props) => props.theme.breakpoints.sm} {
+    font-size: 30px;
+}
 `;
 
 const Desc = styled.p`
